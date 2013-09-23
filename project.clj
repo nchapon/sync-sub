319f8d2f88ff12632e3846369d06065b762c60c2
Nicolas Chapon <nchapon@gmail.com>
2013-09-23 11:52:46 +0200

    Change ouput jar name for uberjar

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject sync-sub "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "See https://github.com/nchapon/sync-sub"
+  :url "https://github.com/nchapon/sync-sub"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [joda-time/joda-time "2.1"]
                  [org.clojure/tools.cli "0.2.4"]
                  [midje "1.5.1"]]
-  :main sync-sub.cmdline)
+  :main sync-sub.cmdline
+  ;; As above, but for uberjar.
+  :uberjar-name "sync-sub.jar")
